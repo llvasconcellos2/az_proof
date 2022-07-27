675a1f61cd1a8bb03bcc6536efbdd094dd45b5e3
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2022-07-27 04:19:42 -0300

    Home bindings and controller

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
 
+import '../../../data/providers/dashboard_provider.dart';
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<HomeController>(() => HomeController(DashboardProvider()));
   }
 }
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,14 +1,22 @@
+import 'package:az_proof/app/data/models/dashboard_model.dart';
 import 'package:get/get.dart';
 
 import '../../../data/preferences/user_preferences.dart';
+import '../../../data/providers/dashboard_provider.dart';
 
 class HomeController extends GetxController {
   final user = UserPreferences().obs;
   final userName = ''.obs;
+  final DashboardProvider dashboardProvider;
+  late DashboardModel dashboardData;
+
+  HomeController(this.dashboardProvider);
 
   @override
   void onInit() async {
     await getName();
+    dashboardData = await dashboardProvider.getAll();
+    print(dashboardData.toJson());
     super.onInit();
   }
 
