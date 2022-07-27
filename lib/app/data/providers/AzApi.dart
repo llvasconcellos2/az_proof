b7274528642405e953f563963467d3d513791ba3
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2022-07-27 18:35:29 -0300

    improved api requests

---
+++ lib/app/data/providers/AzApi.dart
@@ -0,0 +1,73 @@
+import 'package:dio/dio.dart';
+import 'package:html/parser.dart';
+
+import '../preferences/user_preferences.dart';
+
+class AzApi {
+  final baseUrl = 'http://150.230.64.79:9393/proof';
+
+  final options = BaseOptions(
+    followRedirects: false,
+    validateStatus: (status) {
+      return status! < 500;
+    },
+    headers: {'Content-Type': 'application/json'},
+  );
+
+  String error = '';
+
+  static String? _token = null;
+  static void set token(String? value) {
+    if (value != null && value.length > 0) {
+      _token = value;
+    }
+  }
+
+  late final Dio request;
+
+  AzApi() {
+    getTokenFromUserPreferences();
+    request = Dio(options);
+    setInterceptors();
+  }
+
+  void getTokenFromUserPreferences() {
+    var userPreferences = UserPreferences();
+    userPreferences.getToken().then((value) {
+      _token = value;
+    });
+  }
+
+  void setInterceptors() {
+    request.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+      if (_token != null) {
+        options = options.copyWith(
+            headers: Map<String, dynamic>.from(options.headers)
+              ..addAll({'Authorization': 'Bearer ${_token}'}));
+      }
+      handler.next(options);
+    }, onResponse: (response, handler) {
+      if (response.statusCode == 200) {
+        return handler.next(response);
+      } else if (response.statusCode == 404) {
+        // When statusCode is 400 response is HTML
+        error = extractMessageFromHTML(response.data);
+      } else {
+        error = response.data['message'];
+      }
+      handler.reject(DioError(
+        error: error,
+        requestOptions: response.requestOptions,
+        type: DioErrorType.response,
+      ));
+    }, onError: (DioError e, handler) {
+      error = e.message;
+      return handler.next(e); //continue
+    }));
+  }
+
+  String extractMessageFromHTML(String html) {
+    var document = parse(html);
+    return document.getElementsByTagName('pre').first.innerHtml;
+  }
+}
--- lib/app/data/providers/session_provider.dart
+++ lib/app/data/providers/session_provider.dart
@@ -4,51 +4,36 @@ import 'package:dio/dio.dart';
 
 import '../models/user_model.dart';
 import '../preferences/user_preferences.dart';
+import 'AzApi.dart';
 
 class SessionProvider {
-  final request = Dio();
-
-  final opt = Options(
-    followRedirects: false,
-    validateStatus: (status) {
-      return status! < 500;
-    },
-    headers: {'Content-Type': 'application/json'},
-  );
-
-  final baseUrl = 'http://150.230.64.79:9393/proof';
-
   final prefs = UserPreferences();
 
   String _error = '';
   String get error => _error;
+  final AzApi api = AzApi();
 
   Future<bool> signInProvider(String email, String password) async {
     try {
-      final response = await request.post(
-        '$baseUrl/session',
+      final response = await api.request.post(
+        '${api.baseUrl}/session',
         data: jsonEncode(
           {
             "email": email,
             "password": password,
           },
         ),
-        options: opt,
       );
 
-      if (response.statusCode == 200) {
-        var user = UserModel.fromJson(response.data);
+      var user = UserModel.fromJson(response.data);
 
-        await prefs.deleteUser();
-        await prefs.setUser(user);
+      await prefs.deleteUser();
+      await prefs.setUser(user);
+      AzApi.token = user.token;
 
-        return true;
-      } else {
-        _error = response.data['message'];
-        return false;
-      }
-    } catch (e) {
-      _error = e.toString();
+      return true;
+    } on DioError catch (e) {
+      _error = e.message;
       return false;
     }
   }
